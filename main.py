75d042b238a278ccd56bec78d3dca7142f5d76ef
denis <denis.shtykhno@gmail.com>
2023-10-31 21:10:29 +0200

    add file

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
+import random
 
-print('Hellogit')
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.hunger = 0
+        self.energy = 100
+
+    def eat(self, food):
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        print(f"{self.name} is eating. Hunger level: {self.hunger}")
+
+    def sleep(self, hours):
+        self.energy += hours
+        if self.energy > 100:
+            self.energy = 100
+        print(f"{self.name} is sleeping. Energy level: {self.energy}")
+
+    def play(self, time):
+        if self.energy < 10:
+            print(f"{self.name} is too tired to play.")
+        else:
+            self.energy -= time
+            if self.energy < 0:
+                self.energy = 0
+            print(f"{self.name} is playing. Energy level: {self.energy}")
+
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+    def make_sound(self):
+        print(f"{self.name} says Meow!")
+
+class Dog(Animal):
+    def make_sound(self):
+        print(f"{self.name} says Woof!")
+
+# Приклад використання класу:
+if __name__ == "__main__":
+    cat = Cat("Whiskers", "Cat")
+    dog = Dog("Buddy", "Dog")
+
+    for _ in range(3):
+        cat.make_sound()
+        cat.eat(random.randint(5, 15))
+        cat.play(random.randint(10, 30))
+        cat.sleep(random.randint(5, 10))
+
+    for _ in range(3):
+        dog.make_sound()
+        dog.eat(random.randint(10, 20))
+        dog.play(random.randint(15, 40))
+        dog.sleep(random.randint(6, 12))
+
